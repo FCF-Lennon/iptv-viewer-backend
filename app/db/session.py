15e108a36e00eca953832fb51b405b4b11db48fb
pipedev <fcf.remote@gmail.com>
2026-04-14 12:23:58 -0400

    fix: create_engine condicional para SQLite y PostgreSQL (elimina check_same_thread en producción)

--- app/db/session.py
+++ app/db/session.py
@@ -2,10 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import setting
 
-engine = create_engine(
-    setting.database_url, 
-    echo=False, 
-    connect_args={"check_same_thread": False}
+if setting.database_url.startswith("sqlite"):
+    engine = create_engine(
+        setting.database_url,
+        echo=False,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(
+        setting.database_url,
+        echo=False
     )
 
 SessionLocal = sessionmaker(
